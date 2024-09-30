03230a995368b2151b7222f5c562ca5770fcf7a8
Poornaya <sudarshanaiinnovatives@gmail.com>
2024-09-30 19:52:19 +0530

    Add files via upload

---
+++ UI App.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import google.generativeai as genai
+from PIL import Image
+
+genai.configure(api_key="AIzaSyAKbn5qEmQYWTcRxrpqysHdTU9XsoYmdvc")
+
+def get_gemini_repsonse(input, image, prompt):
+    model = genai.GenerativeModel('gemini-1.5-flash')
+    response = model.generate_content([input, image[0], prompt])
+    return response.text
+
+
+def input_image_setup(uploaded_file):
+    # Check if a file has been uploaded
+    if uploaded_file is not None:
+        # Read the file into bytes
+        bytes_data = uploaded_file.getvalue()
+
+        image_parts = [
+            {
+                "mime_type": uploaded_file.type,  # Get the mime type of the uploaded file
+                "data": bytes_data
+            }
+        ]
+        return image_parts
+    else:
+        raise FileNotFoundError("No file uploaded")
+
+
+##initialize our streamlit app
+
+st.set_page_config(page_title="AI User Interface Generator")
+
+st.header("AI UI/UX App")
+input = st.text_input("Enter Language name ", key="input")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+image = ""
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+
+submit = st.button("Generate")
+
+input_prompt = """
+You are an expert in UI/UX coding where you need to examine the image
+               and give a code of that user interface in mentioned coding language  
+"""
+
+## If submit button is clicked
+
+if submit:
+    image_data = input_image_setup(uploaded_file)
+    response = get_gemini_repsonse(input_prompt, image_data, input)
+    st.subheader("The Response is")
+    st.write(response)
